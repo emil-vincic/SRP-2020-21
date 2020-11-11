d823c439c1d410fd5fa58dcd4b860a0fa61b3292
emilvincic <emilvincic@users.noreply.github.com>
2020-11-11 11:35:59 +0100

    Refactor login to issue true JWT tokens

--- postgres-express-node/api/controllers/login.controller.js
+++ postgres-express-node/api/controllers/login.controller.js
@@ -1,20 +1,18 @@
+// login.controller.js
 const winston = require("winston");
 const { loginServiceInstance } = require("../../services");
 
 const Logger = winston.loggers.get("logger");
-
-exports.login = async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const user = await loginServiceInstance.getUser({ username });
-
-    if (user && user.password === password) {
-      return res.json({ message: "Login successful", token: "auth_token" });
+exports.login = async(req, res) => {
+    const { username, password } = req.body;
+    try {
+        const { user, token } = await loginServiceInstance.login({
+            username,
+            password,
+        });
+        return res.json({ user, token });
+    } catch (err) {
+        Logger.error(err);
+        return res.status(400).json({ error: { message: err.message } });
     }
-
-    return res.status(401).json({ error: "Not authorized" });
-  } catch (err) {
-    Logger.error(err);
-    return res.status(400).json({ error: { message: err.message } });
-  }
 };
