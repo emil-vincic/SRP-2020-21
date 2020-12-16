72167f49fc4a493a289071f6cc2ef33f23a293ac
emilvincic <emilvincic@users.noreply.github.com>
2020-12-16 10:31:26 +0100

    Implement online dictionary attack

--- postgres-express-node/loaders/index.js
+++ postgres-express-node/loaders/index.js
@@ -2,10 +2,10 @@ const expressLoader = require("./express");
 const sequelizeLoader = require("./sequelize");
 const { Logger, HttpLogger } = require("./logger");
 
-module.exports = async ({ app }) => {
-  await sequelizeLoader({ Logger });
-  Logger.info("Sequelize loaded (DB connected)");
+module.exports = async({ app }) => {
+    await sequelizeLoader({ Logger });
+    Logger.info("Sequelize loaded (DB connected)");
 
-  expressLoader({ app, HttpLogger });
-  Logger.info("Express app loaded");
+    expressLoader({ app, HttpLogger });
+    Logger.info("Express app loaded");
 };
